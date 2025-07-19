97cd6a283b591be5c07e1bf2e1ac7a1160698255
GLD5000 <garethlouisdevlin@gmail.com>
2025-07-19 09:23:53 +0100

    Script working

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,16 @@ import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ["lib"] ,tsconfigPath: './tsconfig.lib.json'})],
+  plugins: [react(), dts({ include: ["lib"] ,tsconfigPath: './tsconfig.lib.json'}),{
+    name: 'custom-script',
+    enforce: 'post',
+    transform(code, id) {
+      if (id.endsWith('.html')) {
+        return code + `<script>console.log('hello');</script>`;
+      }
+      return code;
+    }
+  }],
   build: {
     lib: {
       entry: resolve(__dirname, "lib/main.ts"),
